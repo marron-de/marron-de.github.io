978c2ba88b4e9c41d768569adfde9491aad5ac0b
marron-de <jeong8545@gmail.com>
2025-05-08 18:29:39 +0900

    수정

--- work/gomyplan/vite.config.js
+++ work/gomyplan/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/work/gomyplan/', 
+  base: '/marron-de.github.io/work/gomyplan/', 
 });
