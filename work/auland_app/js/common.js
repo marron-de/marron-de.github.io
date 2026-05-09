448224c0c4482dd09f3c206565e18f9163a18f23
marron-de <jeong8545@gmail.com>
2026-05-09 19:03:48 +0900

    완료

--- work/auland_app/js/common.js
+++ work/auland_app/js/common.js
@@ -1,23 +1,16 @@
 
-// header navbox button 
+// navmenu button 
 $(document).ready(function () {
 	$(".header .menu_open").on("click", function () {
 		$("body").addClass("hidden");
 		$(".header").addClass("open");
-		$(".navbox").addClass("open");
+		$(".nav_menu").addClass("open");
 	});
-	$(".navbox .menu_close").on("click", function () {
+	$(".nav_menu .menu_close").on("click", function () {
 		$("body").removeClass("hidden");
 		$(".header").removeClass("open");
-		$(".navbox").removeClass("open");
+		$(".nav_menu").removeClass("open");
 	});
-});
-$(document).mouseup(function (e) {
-	if ($(".navbox").has(e.target).length === 0) {
-		$("body").removeClass("hidden");
-		$(".header").removeClass("open");
-		$(".navbox").removeClass("open");
-	}
 });
 
 
@@ -81,7 +74,7 @@ $(document).mouseup(function (e) {
 
 // tab function
 $(document).ready(function () {
-	$(".tab_nav").click(function () {
+	$(".tab_wrap .tab_nav").click(function () {
 		let clickedTab = $(this);
 		let tabWrap = clickedTab.closest(".tab_wrap");
 		let allTabs = tabWrap.find(".tab_nav");
@@ -120,7 +113,7 @@ $(document).ready(function () {
 });
 
 
-/* modal */
+// modal 
 $(".modal .modal_close").click(function () {
 	$("body").removeClass("hidden")
 	$(".modal").removeClass("show")
@@ -130,4 +123,113 @@ $(document).mouseup(function (e) {
 		$("body").removeClass("hidden")
 		$(".modal").removeClass("show")
 	}
+});
+
+
+// tooltip
+$('.alert_icon').on('click', function(e) {
+    e.stopPropagation();
+    $(this).find('.tooltip').toggleClass('show');
+});
+$(document).on('click', function() {
+    $('.tooltip').removeClass('show');
+});
+
+
+// progress width
+function updateProgressWidths() {
+    $('.progressbox .progress').each(function() {
+        const progressWidth = $(this).width();
+        $(this).css('--progress-width', progressWidth + 'px');
+    });
+}
+$(document).ready(function() {
+    updateProgressWidths();
+});
+$(window).on('resize', updateProgressWidths);
+
+
+// 01온보딩_02인트로
+const intro_swiper = new Swiper('.intro_swiper', {
+	loop:true,
+	speed: 500,
+	effect: 'fade',
+	fadeEffect: {
+	crossFade: true
+	},
+	pagination: {
+		el: '.intro_swiper .pagination',
+		clickable: true,
+	},
+});
+
+
+// 01온보딩_05회원가입_02약관동의
+$(document).ready(function() {
+    $('#allCheck').on('change', function() {
+        $('.sub_check').prop('checked', $(this).prop('checked'));
+    });
+    $('.sub_check').on('change', function() {
+        $('#allCheck').prop('checked', $('.sub_check').length === $('.sub_check:checked').length);
+    });
+});
+
+
+// 02검사하기_01홈
+const exam_index = parseInt($('.exam_swiper').attr('data-index'), 10) || 0;
+const exam_swiper = new Swiper('.exam_swiper', {
+    initialSlide: exam_index, // 
+    loop: true,
+    speed: 500,
+    spaceBetween: 40,
+    pagination: {
+        el: '.exam_swiper .pagination',
+        clickable: true,
+    },
+});
+
+
+// 04검사가이드_02저학년검사안내&03고학년검사안내
+$(function() {
+    $('.vidcont').each(function() {
+        const cont = $(this);
+        const videoElement = cont.find('.video');
+        const video = videoElement[0];
+        const playBtn = cont.find('.play_btn, .play_video');
+		
+        const clickTargets = playBtn.add(videoElement);
+
+        clickTargets.on('click', function() {
+            if (video.paused) {
+                video.play();
+            } else {
+                video.pause();
+            }
+        });
+
+        videoElement.on('play', function() {
+            cont.find('.play_btn').addClass('on');
+            cont.find('.play_video').addClass('hide');
+        });
+
+        videoElement.on('pause', function() {
+            cont.find('.play_btn').removeClass('on');
+            cont.find('.play_video').removeClass('hide');
+        });
+
+        videoElement.on('timeupdate', function() {
+            const current = video.currentTime;
+            const duration = video.duration;
+            const progress = (current / duration) * 100;
+
+            cont.find('.bar').css('width', progress + '%');
+            cont.find('.now').text(formatTime(current));
+        });
+
+        function formatTime(seconds) {
+            const min = Math.floor(seconds / 60);
+            const sec = Math.floor(seconds % 60);
+            return min + ":" + (sec < 10 ? '0' + sec : sec);
+        }
+    });
 });
